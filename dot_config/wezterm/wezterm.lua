5bc5a64509acee084898be1867fe6db8aa8094a9
Alan Peabody <gapeabody@gmail.com>
2024-05-05 18:11:43 -0400

    Starting dotfile reboot.

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "tokyonight_night"
+config.window_background_opacity = 1.00
+-- config.font = wezterm.font 'Fira Code'
+config.font_size = 15.0
+
+
+-- Tabs
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+-- and finally, return the configuration to wezterm
+return config
